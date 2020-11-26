ef7f12d66db3946ef1d8c5b9ce22ecd9474414e7
zulfianasyafa <zulfianaauliasyafa@gmail.com>
2020-11-26 23:47:41 +0700

    second commit

--- app/src/main/java/com/syafa/mynavigation/DetailCategoryFragment.kt
+++ app/src/main/java/com/syafa/mynavigation/DetailCategoryFragment.kt
@@ -11,20 +11,32 @@ import kotlinx.android.synthetic.main.fragment_detail_category.*
 
 class DetailCategoryFragment : Fragment() {
 
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_detail_category, container, false)
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        val dataName = arguments?.getString(CategoryFragment.EXTRA_NAME)
-        val dataDescription = arguments?.getLong(CategoryFragment.EXTRA_STOCK)
 
         val dataName = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).name
         val dataDescription = DetailCategoryFragmentArgs.fromBundle(arguments as Bundle).stock
 
         tv_category_name.setText(dataName)
         tv_category_description.setText("Stock : $dataDescription")
-
-        btn_profile.setOnClickListener (
+        btn_profile.setOnClickListener(
             Navigation.createNavigateOnClickListener(R.id.action_detailCategoryFragment_to_homeFragment)
         )
+
     }
 }
--- app/src/main/java/com/syafa/mynavigation/HomeFragment.kt
+++ app/src/main/java/com/syafa/mynavigation/HomeFragment.kt
@@ -20,10 +20,19 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+        }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         btn_category.setOnClickListener(
